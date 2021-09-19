16ad6434980a161ee8521360c6f53584d220d337
fctb12 <f.chalissery@berkeley.edu>
2021-09-18 23:52:28 -0700

    Completed logic for target prompt HTML

--- app.py
+++ app.py
@@ -18,14 +18,18 @@ def upload_file_and_render_target_prompt():
         df = pd.read_csv(uploaded_file.filename)
         column_names = list(df) # ['age', 'gender', 'chest_pain', 'resting_blood_pressure', 'cholesterol', 'fasting_blood_sugar_greater_than_120', 'resting_ecg_result', 'exercise_max_heart_rate', 'exercise_induced_angina', 'exercise_st_depression', 'exercise_st_slope', 'fluoroscopy_vessels_colored', 'thallium_stress_test', 'diagnosis']  
         data = [{"name": column} for column in column_names]
-        return render_template("target_prompts.html", data=data)
+        return render_template("target_prompts.html", data=data, filename=uploaded_file.filename)
     return redirect(url_for('index'))
 
 
-@app.route('/', methods=['POST'])
+@app.route('/train', methods=['POST'])
 def train():
+    target = request.form.get('columns')
+    filename = request.form.get('filename')
+
+    print(target)
+    print(filename)
     # Train the model
-    output = os.system('tangram train --file {} --target diagnosis'.format(uploaded_file.filename))
+    output = os.system('tangram train --file {} --target {}'.format(filename, target))
     print(output)
-    os.remove(uploaded_file.filename)
     return redirect(url_for('index'))
