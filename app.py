02c880abf7a28dd3c10404e341857cb3c6f4080b
fctb12 <f.chalissery@berkeley.edu>
2021-09-18 22:57:10 -0700

    Added target prompt HTML

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
     return render_template('index.html')
 
 @app.route('/', methods=['POST'])
-def upload_file():
+def upload_file_and_render_target_prompt():
     uploaded_file = request.files['file']
     if uploaded_file.filename != '':
         uploaded_file.save(uploaded_file.filename)
@@ -17,9 +17,15 @@ def upload_file():
         # Extract column names
         df = pd.read_csv(uploaded_file.filename)
         column_names = list(df) # ['age', 'gender', 'chest_pain', 'resting_blood_pressure', 'cholesterol', 'fasting_blood_sugar_greater_than_120', 'resting_ecg_result', 'exercise_max_heart_rate', 'exercise_induced_angina', 'exercise_st_depression', 'exercise_st_slope', 'fluoroscopy_vessels_colored', 'thallium_stress_test', 'diagnosis']  
+        data = [{"name": column} for column in column_names]
+        return render_template("target_prompts.html", data=data)
+    return redirect(url_for('index'))
 
-        # Train the model
-        output = os.system('tangram train --file {} --target diagnosis'.format(uploaded_file.filename))
-        print(output)
-        os.remove(uploaded_file.filename)
+
+@app.route('/', methods=['POST'])
+def train():
+    # Train the model
+    output = os.system('tangram train --file {} --target diagnosis'.format(uploaded_file.filename))
+    print(output)
+    os.remove(uploaded_file.filename)
     return redirect(url_for('index'))
