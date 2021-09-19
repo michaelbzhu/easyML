8c04591ca05828c06e4f16d82e31866ab47f0cb3
Merrick Wang <merrickwang1@berkeley.edu>
2021-09-18 22:41:02 -0700

    extract columns

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -12,6 +13,12 @@ def upload_file():
     uploaded_file = request.files['file']
     if uploaded_file.filename != '':
         uploaded_file.save(uploaded_file.filename)
+        
+        # Extract column names
+        df = pd.read_csv(uploaded_file.filename)
+        column_names = list(df)
+        
+        # Train the model
         output = os.system('tangram train --file {} --target diagnosis'.format(uploaded_file.filename))
         print(output)
         os.remove(uploaded_file.filename)
